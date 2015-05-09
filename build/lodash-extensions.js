3daec68773246a203fde8ffe1f1315f4f81c7f1a
Sarah Quigley <sarah@sarahquigley.net>
2015-05-09 13:40:19 +0100

    Improve implementation of _.isEqualIgnoringFxn.

--- build/lodash-extensions.js
+++ build/lodash-extensions.js
@@ -1,21 +1,22 @@
 (function() {
-  _.isEqualIgnoringFxn = function(item, other) {
-    var item_keys;
-    if (typeof item !== typeof other) {
-      return false;
-    } else if (_.isFunction(item)) {
-      return true;
-    } else if (_.isObject(item)) {
-      item_keys = _.keys(item);
-      if (item_keys.length !== _.keys(other).length) {
-        return false;
+  _.isEqualIgnoringFxn = function(value, other) {
+    return _.isEqual(value, other, function(item, other) {
+      var item_keys;
+      if (typeof item === typeof other) {
+        if (_.isFunction(item)) {
+          return true;
+        }
+        if (_.isObject(item)) {
+          item_keys = _.keys(item);
+          if (item_keys.length !== _.keys(other).length) {
+            return false;
+          }
+          return _.all(item_keys, function(key) {
+            return _.isEqualIgnoringFxn(item[key], other[key]);
+          });
+        }
       }
-      return _.all(item_keys, function(key) {
-        return _.isEqualIgnoringFxn(item[key], other[key]);
-      });
-    } else {
-      return _.isEqual(item, other);
-    }
+    });
   };
 
 }).call(this);
--- src/lodash-extensions.coffee
+++ src/lodash-extensions.coffee
@@ -1,15 +1,11 @@
-_.isEqualIgnoringFxn = (item, other) ->
-  if typeof item != typeof other
-    return false
+_.isEqualIgnoringFxn = (value, other) ->
+  _.isEqual value, other, (item, other) ->
+    if typeof item == typeof other
+      if _.isFunction(item)
+        return true
 
-  else if _.isFunction(item)
-    return true
-
-  else if _.isObject(item)
-    item_keys = _.keys(item)
-    return false if item_keys.length != _.keys(other).length
-    return _.all item_keys, (key) ->
-      _.isEqualIgnoringFxn(item[key], other[key])
-
-  else
-    return _.isEqual(item, other)
+      if _.isObject(item)
+        item_keys = _.keys(item)
+        return false if item_keys.length != _.keys(other).length
+        return _.all item_keys, (key) ->
+          _.isEqualIgnoringFxn(item[key], other[key])
